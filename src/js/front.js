ab633c3503514a97df2fd0b45f316b1c281ddb5c
Nazar Kalinchuk <nazariy2410@gmail.com>
2018-10-20 18:02:07 +0300

    blog implement

--- src/js/front.js
+++ src/js/front.js
@@ -2,12 +2,9 @@ let front = {
     hamburger: $('.hamburger'),
     nav: $('.header-list'),
 
-
     init: function () {
         this.events();
-        this.rangeInit();
     },
-
 
     toogleNav: function(){
         if (!this.hamburger.hasClass('is-active')) {
@@ -22,58 +19,6 @@ let front = {
 
 
 
-    getVals: function () {
-        let parent = this.parentNode;
-        let slides = parent.getElementsByTagName("input");
-        let slide1 = parseFloat( slides[0].value );
-        let slide2 = parseFloat( slides[1].value );
-        if( slide1 > slide2 ){
-            slide1 = slide2;
-            slides[0].value = slides[1].value;
-            // let tmp = slide2; slide2 = slide1; slide1 = tmp;
-        }
-        else if (slide2 <= slide1){
-            slide2 = slide1;
-            slides[1].value = slides[0].value;
-        }
-        let displayElement = parent.getElementsByClassName("rangeValues")[0];
-
-
-        displayElement.innerHTML = "$ " + slide1.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + " - $ " + slide2.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-    },
-
-    rangeInit: function(){
-        let sliderSections = document.getElementsByClassName("range");
-        for( let x = 0; x < sliderSections.length; x++ ){
-            let sliders = sliderSections[x].getElementsByTagName("input");
-            for( let y = 0; y < sliders.length; y++ ){
-                if( sliders[y].type ==="range" ){
-                    sliders[y].oninput = this.getVals;
-                    sliders[y].oninput();
-                }
-            }
-        }
-    },
-
-    openTab: function (element, tabName, parent) {
-        let i, tab_content, tab_links;
-
-        tab_content = $(element).closest(parent).find('.tabs-wrap').find('.tab-content');
-        for (i = 0; i < tab_content.length; i++) {
-            tab_content[i].style.display = "none";
-        }
-
-        tab_links = $(element).closest('.tabs-ul').find('.tab-links');
-
-        for (i = 0; i < tab_links.length; i++) {
-            tab_links[i].className = tab_links[i].className.replace(" active", "");
-        }
-
-        document.getElementById(tabName).style.display = "block";
-        $(element).addClass('active');
-    },
-
-
     events: function () {
         let self = this;
 
@@ -82,16 +27,6 @@ let front = {
             self.toogleNav();
         });
 
-        $(document).on('click', '#button_filter_show', function () {
-            $('.search_by_lakes').slideToggle();
-        });
-
-        $(document).on('click', '.drop-arrow', function () {
-           $(this).toggleClass('js-active');
-           $('.sub-menu').toggleClass('js-active');
-        });
-
-
 
     }
 };
@@ -99,7 +34,6 @@ let front = {
 
 
 
-
-jQuery(function () {
+$(function () {
     front.init();
 });
