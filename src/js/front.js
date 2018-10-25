22cb222683cb7a01adea5b1c3c507be33a4ad415
nazariy17 <nazariy2410@gmail.com>
2018-10-25 19:32:51 +0300

    dev

--- src/js/front.js
+++ src/js/front.js
@@ -32,9 +32,53 @@ let front = {
     }
 };
 
+let modal = {
+    closeButton: $('.modal__close'),
+    closeOverlay: $('.modal'),
+    init: function () {
+        this.events();
+    },
+    openModal: function (id) {
+        let modalWindow = $(id);
+        modalWindow.fadeIn();
+        modalWindow.find('.modal__content').removeClass('animate-away').addClass('animate-in');
+        $('body').addClass('--js-modal-animate');
+    },
 
+    closeModal: function (id) {
+        let modalWindow = $(id);
+        modalWindow.find('.modal__content').removeClass('animate-in').addClass('animate-away');
+        modalWindow.fadeOut();
+        $('body').removeClass('--js-modal-animate');
+    },
+
+    events: function () {
+
+        $(document).on('click', '.modalTrigger', function (e) {
+            e.preventDefault();
+            let self = $(this),
+                target = self.attr('data-modal');
+            modal.openModal(target);
+
+        });
+
+        $(document).on('click', '.modal', function (event) {
+            let self = '#' + $(this).attr('id');
+            if (event.target.className == 'modal__body') {
+                modal.closeModal(self);
+            }
+        });
+
+        $(document).on('click', '.modal__close', function () {
+            let self = '#' + $(this).closest('.modal').attr('id');
+            modal.closeModal(self);
+        });
+
+    }
+};
 
 
 $(function () {
     front.init();
+    modal.init();
 });
